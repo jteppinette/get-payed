dc728b0ede35eca333266ae61b3797a373ec6d3c
Josh Eppinette <joshua.eppinette@gtri.gatech.edu>
2016-08-14 16:40:01 -0400

    Add login and register http funcs

--- app/auth/login/login.controller.js
+++ app/auth/login/login.controller.js
@@ -5,8 +5,19 @@
         .module('auth.login')
         .controller('LoginController', LoginController);
 
-    function LoginController() {
+    function LoginController($http) {
+        var vm = this;
 
+        vm.submit = submit;
+
+        vm.credentials = {
+            email: undefined,
+            password: undefined
+        };
+
+        function submit(credentials) {
+            return $http.post("api/auth/login", credentials);
+        }
     }
 
 }());
--- app/auth/register/register.controller.js
+++ app/auth/register/register.controller.js
@@ -5,8 +5,19 @@
         .module('auth.register')
         .controller('RegisterController', RegisterController);
 
-    function RegisterController() {
+    function RegisterController($http) {
+        var vm = this;
 
+        vm.submit = submit;
+
+        vm.credentials = {
+            email: undefined,
+            password: undefined
+        };
+
+        function submit(credentials) {
+            return $http.post("api/auth/register", credentials);
+        }
     }
 
 }());
