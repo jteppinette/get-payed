651874a10c71dd2acb61ed85d3e0ed8d28a44944
Josh Eppinette <joshua.eppinette@gtri.gatech.edu>
2016-08-16 10:30:51 -0400

    Place address required warning on payments page

--- app/dashboard/payments/payments.controller.js
+++ app/dashboard/payments/payments.controller.js
@@ -5,16 +5,18 @@
         .module('dashboard.payments')
         .controller('PaymentsController', PaymentsController);
 
-    function PaymentsController($scope, PaymentsService) {
+    function PaymentsController($scope, PaymentsService, localStorageService) {
         var vm = this;
 
         vm.create = create;
 
         vm.payments = [];
+        vm.address = undefined;
 
         initialize();
 
         function initialize() {
+            vm.address = localStorageService.get('address');
             getList();
             $scope.$on('payments:refresh', getList);
             function getList() {
