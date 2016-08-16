c9277851d82fb6089351bb49a63bb60c7e977226
Josh Eppinette <joshua.eppinette@gtri.gatech.edu>
2016-08-15 22:33:04 -0400

    Show require address alert on overview page

--- app/dashboard/overview/overview.controller.js
+++ app/dashboard/overview/overview.controller.js
@@ -5,14 +5,16 @@
         .module('dashboard.overview')
         .controller('OverviewController', OverviewController);
 
-    function OverviewController($http) {
+    function OverviewController($http, localStorageService) {
         var vm = this;
 
         vm.users = [];
+        vm.address = undefined;
 
         initialize();
 
         function initialize() {
+            vm.address = localStorageService.get('address');
             return $http.get("api/users")
                 .then(function(http) {
                     vm.users = http.data;
