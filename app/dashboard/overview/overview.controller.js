95cc57a9f8837d592e60a4a028d463804a571f51
Josh Eppinette <joshua.eppinette@gtri.gatech.edu>
2016-08-16 10:58:04 -0400

    Remove users from overview

--- app/dashboard/overview/overview.controller.js
+++ app/dashboard/overview/overview.controller.js
@@ -5,20 +5,15 @@
         .module('dashboard.overview')
         .controller('OverviewController', OverviewController);
 
-    function OverviewController($http, localStorageService) {
+    function OverviewController(localStorageService) {
         var vm = this;
 
-        vm.users = [];
         vm.address = undefined;
 
         initialize();
 
         function initialize() {
             vm.address = localStorageService.get('address');
-            return $http.get("api/users")
-                .then(function(http) {
-                    vm.users = http.data;
-                });
         }
     }
 
